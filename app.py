8aec7213fc841873fd8077b014e81545cb57d4a6
vika1018 <92905298+vika1018@users.noreply.github.com>
2021-11-09 17:19:05 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -31,22 +31,12 @@ line_bot_api = LineBotApi('hmDdVp5SnY9VouuOj0x72AsaMqbTed27zRlnvL6shzvf+CZElkGgG
  
 # 必須放上自己的Channel Secret
 handler = WebhookHandler('7e2526361af1800f787f38d7de4ab065')
-line_bot_api.push_message('U535ea272631c1d15ea420c3f1db6332c', TextSendMessage(text='Hi~心花開來嘍ΘΘ'))
+line_bot_api.push_message('U535ea272631c1d15ea420c3f1db6332c', TextSendMessage(text='Hi~心花開來嘍ΘΘ感謝你加心花開好友,現在起你就是我的主人,主人可以吩咐我做任何事唷'))
 
 #======MongoDB操作範例======
 @handler.add(PostbackEvent)
 def handle_message(event):
     print(event.postback.data)
-
-
-@handler.add(MemberJoinedEvent)
-def welcome(event):
-    uid = event.joined.members[0].user_id
-    gid = event.source.group_id
-    profile = line_bot_api.get_group_member_profile(gid, uid)
-    name = profile.display_name
-    message = TextSendMessage(text=f'{name}感謝你加心花開好友,現在起你就是我的主人,主人可以吩咐我做任何事唷^^')
-    line_bot_api.reply_message(event.reply_token, message)
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -71,14 +61,14 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)    
 def handle_message(event):
     message = TextMessage (text="你是男是女")
-    message = TextMessage (text="主人想要我是男的,我就男的,而且可以當主人的暖男!主人要是想要我是女的,我就是女的,而且可以當主人的小甜心唷~")
+    message = TextMessage (event.message.text+"主人想要我是男的,我就男的,而且可以當主人的暖男!主人要是想要我是女的,我就是女的,而且可以當主人的小甜心唷~")
     line_bot_api.reply_message(event.reply_token,message)
 @handler.add(MessageEvent, message=TextMessage)    
 def handle_message(event):
     message = TextMessage (text="心花開")
-    message = TextMessage (text="主人我在~有何吩咐")
+    message = TextMessage (event.message.text+"主人我在~有何吩咐")
     line_bot_api.reply_message(event.reply_token,message) 
-       
+
     #主程式
 import os
 if __name__ == "__main__":
